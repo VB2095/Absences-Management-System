391401f33a6880f65e6556355a40ded9931e3d11
leviatan89 <leviatan.89@gmail.com>
2016-02-09 09:23:40 +0000

    Delete default user/pasword per one general.

--- includes/conectar_bd.php
+++ includes/conectar_bd.php
@@ -3,7 +3,7 @@
 //Utilizamos PDO para evitar el SQLInjection.
 function conectarBD($bd) {
 	try{
-		$dbConnection = new PDO('mysql:dbname='.$bd.';host=127.0.0.1;charset=utf8', 'balmis_asir', '12345qwert');
+		$dbConnection = new PDO('mysql:dbname='.$bd.';host=127.0.0.1;charset=utf8', 'user', 'password');
 		$dbConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 		$dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}catch(PDOException $e){
